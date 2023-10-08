8566c62cf54a2e2ec60ab8a5101a03544f383a98
Duy Minh <duyminh081201@gmail.com>
2023-10-08 16:17:48 +0700

    Revert "check null or undefined"
    This reverts commit c7674472da59752953cc89ff7831a74c5ede18b9.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,51 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document?.getElementById("nav-menu"),
-  navToggle = document?.getElementById("nav-toggle"),
-  navClose = document?.getElementById("nav-close");
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
-navToggle?.addEventListener("click", () => {
-  navMenu.classList.add("show-menu");
-});
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 /*===== MENU HIDDEN =====*/
-navClose?.addEventListener("click", () => {
-  navMenu?.classList?.remove("show-menu");
-});
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document?.querySelectorAll(".nav__link");
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-  const navMenu = document?.getElementById("nav-menu");
+  const navMenu = document.getElementById("nav-menu");
   // When we click on each nav__link, we remove the show-menu class
-  navMenu?.classList?.remove("show-menu");
+  navMenu.classList.remove("show-menu");
 };
-navLink?.forEach((n) => n?.addEventListener("click", linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== SHADOW HEADER ===============*/
 const shadowHeader = () => {
-  const header = document?.getElementById("header");
+  const header = document.getElementById("header");
   // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
   this.scrollY >= 50
-    ? header?.classList?.add("shadow-header")
-    : header?.classList?.remove("shadow-header");
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
 };
 window.addEventListener("scroll", shadowHeader);
 /*=============== EMAIL JS ===============*/
-const contactForm = document?.getElementById("contact-form");
-const contactMessage = document?.getElementById("contact-message");
-const btnSendMessage = document?.getElementById("contact-button");
+const contactForm = document.getElementById("contact-form");
+const contactMessage = document.getElementById("contact-message");
+const btnSendMessage = document.getElementById("contact-button");
 
 const sendEmail = (e) => {
-  e?.preventDefault();
-  btnSendMessage?.innerHTML = "<i class='ri-send-plane-line'></i> Sending...";
-  btnSendMessage?.disabled = true;
+  e.preventDefault();
+  btnSendMessage.innerHTML = "<i class='ri-send-plane-line'></i> Sending...";
+  btnSendMessage.disabled = true;
   // service id, template id, form, public key
   emailjs
     .sendForm(
@@ -50,59 +56,59 @@ const sendEmail = (e) => {
     )
     .then(
       (result) => {
-        contactMessage?.innerHTML = "Message sent successfully ✅";
+        contactMessage.innerHTML = "Message sent successfully ✅";
         setTimeout(() => {
-          contactMessage?.innerHTML = "";
+          contactMessage.innerHTML = "";
         }, 5000);
         // reset form
-        contactForm?.reset();
-        btnSendMessage?.innerHTML =
+        contactForm.reset();
+        btnSendMessage.innerHTML =
           "<i class='ri-send-plane-line'></i> Send Message";
-        btnSendMessage?.disabled = false;
+        btnSendMessage.disabled = false;
       },
       (error) => {
-        contactMessage?.innerHTML = "Message not sent (service error) ❌";
+        contactMessage.innerHTML = "Message not sent (service error) ❌";
         setTimeout(() => {
-          contactMessage?.innerHTML = "";
+          contactMessage.innerHTML = "";
         }, 5000);
-        btnSendMessage?.innerHTML =
+        btnSendMessage.innerHTML =
           "<i class='ri-send-plane-line'></i> Send Message";
-        btnSendMessage?.disabled = false;
+        btnSendMessage.disabled = false;
       }
     );
 };
 
-contactForm?.addEventListener("submit", sendEmail);
+contactForm.addEventListener("submit", sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
-  const scrollUp = document?.getElementById("scroll-up");
+  const scrollUp = document.getElementById("scroll-up");
   // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-  this?.scrollY >= 350
-    ? scrollUp?.classList?.add("show-scroll")
-    : scrollUp?.classList?.remove("show-scroll");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
 };
 
-window?.addEventListener("scroll", scrollUp);
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document?.querySelectorAll("section[id]");
+const sections = document.querySelectorAll("section[id]");
 
 const scrollActive = () => {
-  const scrollDown = window?.scrollY;
+  const scrollDown = window.scrollY;
 
-  sections?.forEach((current) => {
-    const sectionHeight = current?.offsetHeight,
-      sectionTop = current?.offsetTop - 58,
-      sectionId = current?.getAttribute("id"),
-      sectionsClass = document?.querySelector(
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
         ".nav__menu a[href*=" + sectionId + "]"
       );
 
     if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-      sectionsClass?.classList?.add("active-link");
+      sectionsClass.classList.add("active-link");
     } else {
-      sectionsClass?.classList?.remove("active-link");
+      sectionsClass.classList.remove("active-link");
     }
   });
 };
@@ -110,36 +116,36 @@ window.addEventListener("scroll", scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
 // get current theme of browser
-const isDarkTheme = window?.matchMedia("(prefers-color-scheme: dark)");
-const themeButton = document?.getElementById("theme-button");
+const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+const themeButton = document.getElementById("theme-button");
 
 const darkTheme = () => {
   // Add or remove the dark / icon theme
-  document?.body.classList?.add("dark-theme");
-  themeButton?.classList?.add("ri-moon-line");
-  themeButton?.classList?.remove("ri-sun-line");
+  document.body.classList.add("dark-theme");
+  themeButton.classList.add("ri-moon-line");
+  themeButton.classList.remove("ri-sun-line");
 };
 
 const lightTheme = () => {
   // Add or remove the dark / icon theme
-  document?.body?.classList?.remove("dark-theme");
-  themeButton?.classList?.remove("ri-moon-line");
-  themeButton?.classList?.add("ri-sun-line");
+  document.body.classList.remove("dark-theme");
+  themeButton.classList.remove("ri-moon-line");
+  themeButton.classList.add("ri-sun-line");
 };
 
-isDarkTheme?.matches ? darkTheme() : lightTheme();
+isDarkTheme.matches ? darkTheme() : lightTheme();
 
 // Detect the dark mode
-isDarkTheme?.addEventListener("change", () => {
-  isDarkTheme?.matches ? darkTheme() : lightTheme();
+isDarkTheme.addEventListener("change", () => {
+  isDarkTheme.matches ? darkTheme() : lightTheme();
 });
 
 // Activate / deactivate the theme manually with the button
-themeButton?.addEventListener("click", () => {
+themeButton.addEventListener("click", () => {
   // Add or remove the dark / icon theme
-  document?.body?.classList?.toggle("dark-theme");
-  themeButton?.classList?.toggle("ri-moon-line");
-  themeButton?.classList?.toggle("ri-sun-line");
+  document.body.classList.toggle("dark-theme");
+  themeButton.classList.toggle("ri-moon-line");
+  themeButton.classList.toggle("ri-sun-line");
 });
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
@@ -150,14 +156,12 @@ const sr = ScrollReveal({
   // reset: true, // to repeat animation
 });
 
-sr?.reveal(
+sr.reveal(
   ".home__perfil, .about__image, .contact__mail, #experience-box-right, #music-box-right",
   { origin: "right" }
 );
-sr?.reveal(
+sr.reveal(
   ".home__name, .home__info, .about__container, .section__title-1, .about__info, .contact__social, .contact__data, #experience-box-left, #music-box-left",
   { origin: "left" }
 );
-sr?.reveal(".projects__card, .section__title-2, .music__title-sub", {
-  interval: 100,
-});
+sr.reveal(".projects__card, .section__title-2, .music__title-sub", { interval: 100 });
